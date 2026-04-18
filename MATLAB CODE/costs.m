b58b3f2246c0a872832ae0d6440b6006f06160a4
Markus Woerle <markus.woerle@iwb.tum.de>
2026-04-18 16:19:33 +0200

    Add files via upload

--- MATLAB CODE/costs.m
+++ MATLAB CODE/costs.m
@@ -7,10 +7,10 @@
 function system_costs = costs(electrolyzer_powers,hydrogen_demand_profile_cycle_representative,number_of_data_points_per_cycle)
     
     % Specifying relevant cost parameters
-    specific_electrolyzer_capex = 500; % in EUR/kW // Configurations: 500, 1000, 1500
-    specific_electrolyzer_opex = 0.03; % in %/a
+    specific_electrolyzer_capex = 1000; % in EUR/kW // Configurations: 500, 1000, 1500
+    specific_electrolyzer_opex = 0.03; % in 1/a
     specific_storage_capex = 100; % in EUR/Nm^3
-    specific_storage_opex = 0.015; % in %/a
+    specific_storage_opex = 0.015; % in 1/a
     service_life = 10; % in a
     
     % Calling the function to calculate the necessary storage capacity for a given electrolyzer power
